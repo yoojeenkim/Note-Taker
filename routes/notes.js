b328bf83c8d120218843a7908f247103410ce5ad
daniel <yoojeenkim@gmail.com>
2021-10-14 13:20:56 -0400

    working note route

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,28 @@
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request received to get notes.`);
+const route = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
-    console.info(`${req.method} request received to get notes`);
+route.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+route.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added succesfully!`);
+    } else {
+        res.error('Error in adding note')
+    }
+});
+
+module.exports = route;
--- server.js
+++ server.js
@@ -20,4 +20,8 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
