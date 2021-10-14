1a2446ebc3d91ff4b392d135f6979aae03623f83
daniel <yoojeenkim@gmail.com>
2021-10-14 09:59:34 -0400

    started route for retrieving notes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -14,5 +18,11 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(`${req.method} request received to get notes.`);
+
+    console.info(`${req.method} request received to get notes`);
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
