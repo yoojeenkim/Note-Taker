9ff28bcf6d7ab4c92679bb74037685acbe67c8d0
daniel <yoojeenkim@gmail.com>
2021-10-14 20:53:25 -0400

    added a /

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
